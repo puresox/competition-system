75c614756c43c9afcf40b851ae0f8903dd97d0c3
puresox <13004384112@163.com>
2017-07-07 20:52:30 +0800

    GET /manage/:competitionId/raters GET /manage/:competitionId/items GET /manage/:competitionId/participants GET /manage/:competitionId/scores

--- routes/manage.js
+++ routes/manage.js
@@ -3,6 +3,9 @@ const checkLogin = require('../middlewares/check').checkLogin;
 const checkAdmin = require('../middlewares/check').checkAdmin;
 const competitionModels = require('../lib/mongo').Competition;
 const userModels = require('../lib/mongo').User;
+const itemModels = require('../lib/mongo').Item;
+const participantModels = require('../lib/mongo').Participant;
+const scoreModels = require('../lib/mongo').Score;
 
 // GET /manage
 router.get('/', checkLogin, checkAdmin, (req, res) => {
@@ -18,9 +21,54 @@ router.get('/:competitionId/hosts', checkLogin, checkAdmin, (req, res) => {
   userModels.find({
     role: 1,
     competition: competitionId,
+  }, 'name', (err, hosts) => {
+    req.flash('error', err);
+    res.render('manage/hosts', { hosts });
+  });
+});
+
+// GET /manage/:competitionId/raters
+router.get('/:competitionId/raters', checkLogin, checkAdmin, (req, res) => {
+  const competitionId = req.params.competition;
+  userModels.find({
+    role: 2,
+    competition: competitionId,
   }, 'name', (err, raters) => {
     req.flash('error', err);
-    res.render('manage/hosts', { raters });
+    res.render('manage/raters', { raters });
+  });
+});
+
+// GET /manage/:competitionId/items
+router.get('/:competitionId/items', checkLogin, checkAdmin, (req, res) => {
+  const competitionId = req.params.competition;
+  itemModels.find({
+    competition: competitionId,
+  }, (err, items) => {
+    req.flash('error', err);
+    res.render('manage/items', { items });
+  });
+});
+
+// GET /manage/:competitionId/participants
+router.get('/:competitionId/participants', checkLogin, checkAdmin, (req, res) => {
+  const competitionId = req.params.competition;
+  participantModels.find({
+    competition: competitionId,
+  }, (err, participants) => {
+    req.flash('error', err);
+    res.render('manage/participants', { participants });
+  });
+});
+
+// GET /manage/:competitionId/scores
+router.get('/:competitionId/scores', checkLogin, checkAdmin, (req, res) => {
+  const competitionId = req.params.competition;
+  scoreModels.find({
+    competition: competitionId,
+  }, (err, scores) => {
+    req.flash('error', err);
+    res.render('manage/scores', { scores });
   });
 });
 
