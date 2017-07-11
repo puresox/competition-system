0f2a3744e2afb9745de49bbfb390cffa33e02195
BernardLi <bernardli1248@qq.com>
2017-07-11 19:33:29 +0800

    feat:主持人的增删查改

---
+++ public/js/host.js
@@ -0,0 +1,13 @@
+new Vue({
+    el: '#demo',
+    data: {
+        show: true
+    }
+})
+
+var app = new Vue({
+    el: '#example-2',
+    data: {
+        show: true
+    }
+})
--- public/js/rater.js
+++ public/js/rater.js
@@ -1,66 +1,131 @@
+// const player = {
+//     props: ['player'],
+//     template: '#test',
+//     data: function () {
+//         return {
+//             data: '哈哈'
+//         }
+//     },
+//     watch: {
+//         '$route': 'getNew'
+//     },
+//     mounted: function () {
+//         this.userId = this.$route.params.name
+//     },
+//     methods: {
+//         getNew: function () {
+//             this.data = this.player['1']
+//         }
+//     }
+// }
+
+// const router = new VueRouter({
+//     routes: [
+//         { path: '/player/:name', component: player }
+//     ]
+// })
+
+// Vue.component('player-panel', {
+//     template: '#test'
+// })
+
+// var vue = new Vue({
+//     router,
+//     el: '#spa',
+//     data: {
+//         message: '测试',
+//         routes: [{
+//             name: '一号选手',
+//             url: '/player/一号选手'
+//         }, {
+//             name: '二号选手',
+//             url: '/player/二号选手'
+//         }, {
+//             name: '三号选手',
+//             url: '/player/三号选手'
+//         }],
+//         playerData: {}
+//     },
+//     created: function () {
+//         // 假设这个是获取的选手数据
+//         var playerData = [{
+//             name: '小马智停',
+//             order: 2
+//         }, {
+//             name: '大棚',
+//             order: 3
+//         }, {
+//             name: '视翼VR',
+//             order: 1
+//         }]
+//         for (let i = 0, len = playerData.length; i < len; i++) {
+//             this.playerData[playerData[i].order.toString()] = playerData[i]
+//         }
+//         console.log(this.playerData)
+//     }
+// })
+
 const player = {
-    props: ['player'],
+    props: ['players'],
     template: '#test',
     data: function () {
         return {
-            data: '哈哈'
+            playerData: '',
+            userId: ''
         }
     },
-    watch: {
-        '$route': 'getNew'
+    created: function () {
+        this.playerData = this.players[this.$route.params.order - 1]
     },
-    mounted: function () {
-        this.userId = this.$route.params.name
+    // 监听路由的变化
+    watch: {
+        '$route': 'updateData'
     },
     methods: {
-        getNew: function () {
-            this.data = this.player['1']
+        updateData: function () {
+            this.playerData = this.players[this.$route.params.order - 1]
         }
     }
 }
 
 const router = new VueRouter({
     routes: [
-        { path: '/player/:name', component: player }
+        { path: '/player/:order', component: player }
     ]
-})
-
-Vue.component('player-panel', {
-    template: '#test'
 })
 
 var vue = new Vue({
     router,
     el: '#spa',
     data: {
-        message: '测试',
-        routes: [{
-            name: '一号选手',
-            url: '/player/一号选手'
-        }, {
-            name: '二号选手',
-            url: '/player/二号选手'
-        }, {
-            name: '三号选手',
-            url: '/player/三号选手'
-        }],
-        playerData: {}
+        message: '',
+        routes: [],
+        players: []
     },
     created: function () {
         // 假设这个是获取的选手数据
-        var playerData = [{
+        var data = [{
             name: '小马智停',
+            intro: '这部分是介绍',
             order: 2
         }, {
             name: '大棚',
+            intro: '这部分是介绍',
             order: 3
         }, {
             name: '视翼VR',
+            intro: '这部分是介绍',
             order: 1
         }]
-        for (let i = 0, len = playerData.length; i < len; i++) {
-            this.playerData[playerData[i].order.toString()] = playerData[i]
+        // todo:不知道弃权的队伍会不会有序号，可能要加个判断
+        for (let i = 0, len = data.length; i < len; i++) {
+            // 绑定获取到的数据到Vue实例的data上
+            this.players[data[i].order - 1] = data[i]
+            // 设置每个参赛项目的url
+            this.routes[data[i].order - 1] = {
+                name: data[i].name,
+                url: '/player/' + data[i].order
+            }
         }
-        console.log(this.playerData)
     }
 })
