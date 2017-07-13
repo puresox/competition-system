3694a3bbbb1729b20afa83bc8cbe7e35f2a430f2
puresox <13004384112@163.com>
2017-07-13 21:42:37 +0800

    socketio

--- socketio.js
+++ socketio.js
@@ -3,16 +3,34 @@ const socketio = require('socket.io');
 module.exports = (server) => {
   const io = socketio.listen(server);
 
-  io.on('connection', (socket) => {
-    socket
-      .broadcast
-      .emit('hi');
-    io.emit('chat message', 'system:a user connected');
-    socket.on('chat message', (msg) => {
-      io.emit('chat message', msg);
+  const host = io.of('/host');
+  const screen = io.of('/screen');
+  const rater = io.of('/rater');
+
+  host.on('connection', (socket) => {
+    // 抽签
+    socket.on('drawn', () => {
+      screen.emit('getStatus');
     });
-    socket.on('disconnect', () => {
-      io.emit('chat message', 'system:user disconnected');
+    // 开始比赛
+    socket.on('begin', () => {
+      screen.emit('begin');
+    });
+  });
+
+  screen.on('connection', (socket) => {
+    socket.emit('chat message', 'hello master');
+    screen.emit('chat message', 'system');
+    socket.on('chat message', (msg) => {
+      screen.emit('chat message', msg);
+    });
+  });
+
+  rater.on('connection', (socket) => {
+    socket.emit('chat message', 'hello master');
+    rater.emit('chat message', 'system');
+    socket.on('chat message', (msg) => {
+      rater.emit('chat message', msg);
     });
   });
 };
