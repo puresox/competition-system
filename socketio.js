0e553dccb7e911aa3b788cfa0df486269ca3291e
puresox <puresox@163.com>
2017-10-05 21:57:18 +0800

    1.显示总分。 2.显示获奖名单。

--- socketio.js
+++ socketio.js
@@ -1,119 +1,126 @@
-const moment = require('moment');
+const moment = require('moment')
 
 module.exports = (io) => {
-  const host = io.of('/host');
-  const screen = io.of('/screen');
-  const rater = io.of('/rater');
-  const countDown = io.of('/countDown');
-  const management = io.of('/management');
+  const host = io.of('/host')
+  const screen = io.of('/screen')
+  const rater = io.of('/rater')
+  const countDown = io.of('/countDown')
+  const management = io.of('/management')
 
   const onlineCount = {
     host: 0,
     screen: 0,
     rater: 0,
-    countDown: 0,
-  };
+    countDown: 0
+  }
 
   host.on('connection', (socket) => {
-    onlineCount.host += 1;
+    onlineCount.host += 1
     socket.on('disconnect', () => {
-      onlineCount.host -= 1;
-      management.emit('updateCount', onlineCount);
-    });
-    management.emit('updateCount', onlineCount);
+      onlineCount.host -= 1
+      management.emit('updateCount', onlineCount)
+    })
+    management.emit('updateCount', onlineCount)
     // 开始自动抽签
     socket.on('autoDraw', () => {
-      screen.emit('autoDraw');
-    });
+      screen.emit('autoDraw')
+    })
     // 开始手动抽签
     socket.on('manuDraw', () => {
-      screen.emit('manuDraw');
-    });
+      screen.emit('manuDraw')
+    })
     // 手动抽签完成
     socket.on('manuDrawn', () => {
-      rater.emit('manuDrawn');
-      screen.emit('manuDrawn');
-    });
+      rater.emit('manuDrawn')
+      screen.emit('manuDrawn')
+    })
     // 开始展示参赛作品
     socket.on('nextParticipant', () => {
-      screen.emit('nextParticipant');
-      rater.emit('nextParticipant');
-    });
+      screen.emit('nextParticipant')
+      rater.emit('nextParticipant')
+    })
     // 开始评分
     socket.on('beginScore', () => {
-      screen.emit('beginScore');
-      rater.emit('beginScore');
-    });
+      screen.emit('beginScore')
+      rater.emit('beginScore')
+    })
+    // 显示总分
+    socket.on('showTotalScore', () => {
+      screen.emit('showTotalScore')
+    })
     // 结束比赛
     socket.on('end', () => {
-      screen.emit('end');
-      rater.emit('end');
-    });
+      screen.emit('end')
+      rater.emit('end')
+    })
     // 倒计时
     socket.on('countDown', () => {
-      const minutes = moment('05:00', 'mm:ss');
-      const intervalID = setInterval(animate, 1000);
-      function animate() {
-        minutes.subtract(1, 'seconds');
-        countDown.emit('countDown', minutes.format('mm:ss'));
+      const minutes = moment('05:00', 'mm:ss')
+      const intervalID = setInterval(animate, 1000)
+      function animate () {
+        minutes.subtract(1, 'seconds')
+        countDown.emit('countDown', minutes.format('mm:ss'))
         if (minutes.format('mm:ss') === '00:00') {
-          clearInterval(intervalID);
+          clearInterval(intervalID)
         }
       }
       socket.on('endCountDown', () => {
-        clearInterval(intervalID);
-        countDown.emit('countDown', '05:00');
-      });
-    });
+        clearInterval(intervalID)
+        countDown.emit('countDown', '05:00')
+      })
+    })
     // 显示排名
     socket.on('showRanking', () => {
-      screen.emit('showRanking');
-    });
+      screen.emit('showRanking')
+    })
     // 隐藏排名
     socket.on('hideRanking', () => {
-      screen.emit('hideRanking');
-    });
-  });
+      screen.emit('hideRanking')
+    })
+    // 显示获奖信息
+    socket.on('showWinners', () => {
+      screen.emit('showWinners')
+    })
+  })
 
   screen.on('connection', (socket) => {
-    onlineCount.screen += 1;
+    onlineCount.screen += 1
     socket.on('disconnect', () => {
-      onlineCount.screen -= 1;
-      management.emit('updateCount', onlineCount);
-    });
-    management.emit('updateCount', onlineCount);
+      onlineCount.screen -= 1
+      management.emit('updateCount', onlineCount)
+    })
+    management.emit('updateCount', onlineCount)
     // 自动抽签完成
     socket.on('autoDrawn', () => {
       // rater.emit('autoDrawn');
-      host.emit('autoDrawn');
-    });
+      host.emit('autoDrawn')
+    })
     // 该参赛作品评分结束
     socket.on('endParticipant', () => {
-      host.emit('endParticipant');
-      rater.emit('endParticipant');
-    });
-  });
+      host.emit('endParticipant')
+    })
+  })
 
   rater.on('connection', (socket) => {
-    onlineCount.rater += 1;
+    onlineCount.rater += 1
     socket.on('disconnect', () => {
-      onlineCount.rater -= 1;
-      management.emit('updateCount', onlineCount);
-    });
-    management.emit('updateCount', onlineCount);
+      onlineCount.rater -= 1
+      management.emit('updateCount', onlineCount)
+    })
+    management.emit('updateCount', onlineCount)
     // 评分结束
     socket.on('endScore', (score) => {
-      screen.emit('endScore', score);
-      host.emit('endScore', score);
-    });
-  });
+      screen.emit('endScore', score)
+      host.emit('endScore', score)
+    })
+  })
 
   countDown.on('connection', (socket) => {
-    onlineCount.countDown += 1;
+    onlineCount.countDown += 1
     socket.on('disconnect', () => {
-      onlineCount.countDown -= 1;
-      management.emit('updateCount', onlineCount);
-    });
-    management.emit('updateCount', onlineCount);
-  });
-};
+      onlineCount.countDown -= 1
+      management.emit('updateCount', onlineCount)
+    })
+    management.emit('updateCount', onlineCount)
+  })
+}
